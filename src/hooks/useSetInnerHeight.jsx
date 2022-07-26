574c5b85b3e566e49e9f96922b385bbca56e2e1e
Chadly Riedewald <no925.c@gmail.com>
2022-07-26 02:48:59 +0200

    Crew page fully responsive

--- src/hooks/useSetInnerHeight.jsx
+++ src/hooks/useSetInnerHeight.jsx
@@ -1,13 +1,16 @@
 import { useLayoutEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 
 const useSetInnerHeight = () => {
+    const location = useLocation();
+
     useLayoutEffect(() => {
         const windowInnerHeight = window.innerHeight;
         document.documentElement.style.setProperty(
             '--windowInnerHeight',
             `${windowInnerHeight}px`
         );
-    }, []);
+    }, [location]);
 
     return null;
 };
